0e9183f210cd12393776ec9a7e19487bccf08a39
Adityarockyroy <adityarockyroy136@gmail.com>
2025-08-01 23:11:35 +0530

    updating the base class

--- src/test/java/testBase/baseClass.java
+++ src/test/java/testBase/baseClass.java
@@ -1,9 +1,9 @@
 package testBase;
 
 import java.time.Duration;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -13,37 +13,57 @@ import org.testng.annotations.Parameters;
 
 public class baseClass {
 
-	public static WebDriver driver;
-	
-	public static WebDriver getDriver() {
-		return driver;
-	}
-	@Parameters({"browser"})
-	@BeforeTest(alwaysRun = true)
-	public void setUp(String browser) {
-		
-		switch (browser.toLowerCase()) {
-			case "chrome":
-				driver = new ChromeDriver();
-				break;
-			case "firefox":
-				driver = new FirefoxDriver();
-				break;
-			case "edge":
-				System.setProperty("webdriver.edge.driver", "D:\\edgedriver_win64\\msedgedriver.exe");
-				EdgeOptions edgeOptions = new EdgeOptions();
-				driver = new EdgeDriver(edgeOptions);
-				break;
-			default:
-				throw new IllegalArgumentException("Browser not supported: " + browser);
-		}
-		driver.manage().window().maximize();
-		driver.get("https://tutorialsninja.com/demo/");
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-	}
-	
-	@AfterTest
-	public void TearDown() {
-		driver.quit();
-	}
+    public static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    @Parameters({"browser"})
+    @BeforeTest(alwaysRun = true)
+    public void setUp(String browser) {
+
+        boolean isJenkins = System.getenv("JENKINS_HOME") != null;
+
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                ChromeOptions chromeOptions = new ChromeOptions();
+                if (isJenkins) {
+                    chromeOptions.addArguments("--headless=new");
+                    chromeOptions.addArguments("--no-sandbox");
+                    chromeOptions.addArguments("--disable-dev-shm-usage");
+                    chromeOptions.addArguments("--remote-allow-origins=*");
+                }
+                driver = new ChromeDriver(chromeOptions);
+                break;
+
+            case "firefox":
+                driver = new FirefoxDriver(); // Add headless support if needed
+                break;
+
+            case "edge":
+                System.setProperty("webdriver.edge.driver", "D:\\edgedriver_win64\\msedgedriver.exe");
+                EdgeOptions edgeOptions = new EdgeOptions();
+                if (isJenkins) {
+                    edgeOptions.addArguments("--headless=new");
+                    edgeOptions.addArguments("--no-sandbox");
+                    edgeOptions.addArguments("--disable-dev-shm-usage");
+                    edgeOptions.addArguments("--remote-allow-origins=*");
+                }
+                driver = new EdgeDriver(edgeOptions);
+                break;
+
+            default:
+                throw new IllegalArgumentException("Browser not supported: " + browser);
+        }
+
+        driver.manage().window().maximize();
+        driver.get("https://tutorialsninja.com/demo/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterTest
+    public void TearDown() {
+        driver.quit();
+    }
 }
