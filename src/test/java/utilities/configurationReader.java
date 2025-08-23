c9a5cefca09c2f1fe7bf56d2117ebfa2383bc368
Adityarockyroy <adityarockyroy136@gmail.com>
2025-08-23 18:46:40 +0530

    Latest updates, ignoring .env file

--- src/test/java/utilities/configurationReader.java
+++ src/test/java/utilities/configurationReader.java
@@ -1,11 +1,24 @@
 package utilities;
 
+import java.io.File;
+
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class configurationReader {
+    private static Dotenv dotenv;
 
-	private static final Dotenv dotenv = Dotenv.configure().filename("configuration.env") 
-			.load();
+
+	static {
+        File envFile = new File(".env");
+
+        if (envFile.exists()) {
+            dotenv = Dotenv.configure().filename(".env").load();
+            System.out.println(" Loaded environment variables from .env");
+        } else {
+            dotenv = Dotenv.configure().filename(".env.example").load();
+            System.out.println(" .env not found, using .env.example instead");
+        }
+    }
 
 	public static String get(String key) {
 		return dotenv.get(key);
