2f80adda446fe2460ce7c787ce16dbd43fb5c4aa
Adityarockyroy <adityarockyroy136@gmail.com>
2025-12-22 22:28:02 +0530

    UpdatedBaseClass

--- src/test/java/testBase/baseClass.java
+++ src/test/java/testBase/baseClass.java
@@ -30,177 +30,227 @@ import utilities.configurationReader;
 
 public class baseClass {
 
-    // ThreadLocal WebDriver
-    private static final ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+	// ThreadLocal WebDriver
+	private static final ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
 
-    // still expose driver, so no need to change test/page code
-    public static WebDriver driver;
+	// still expose driver, so no need to change test/page code
+	public static WebDriver driver;
 
-    /** 
-     * @param driver
-     */
-    private static void setDriver(WebDriver driver) {
-        tlDriver.set(driver);
-    }
+	/**
+	 * @param driver
+	 */
+	private static void setDriver(WebDriver driver) {
+		tlDriver.set(driver);
+	}
 
-    /** 
-     * @return WebDriver
-     */
-    public static WebDriver getDriver() {
-        return tlDriver.get();
-    }
+	/**
+	 * @return WebDriver
+	 */
+	public static WebDriver getDriver() {
+		return tlDriver.get();
+	}
 
-    /** 
-     * @param browser
-     * @throws MalformedURLException
-     */
-    @Parameters({"browser"})
-    @BeforeTest(alwaysRun = true)
-    public void setUp(String browser) throws MalformedURLException {
+	/**
+	 * @param browser
+	 * @throws MalformedURLException
+	 */
+	@Parameters({ "browser" })
+	@BeforeTest(alwaysRun = true)
+	public void setUp(String browser) throws MalformedURLException {
 
-        LoggerLoad.info("===== Test Execution Started =====");
-        LoggerLoad.info("Selected Browser: " + browser);
+		LoggerLoad.info("===== Test Execution Started =====");
+		LoggerLoad.info("Selected Browser: " + browser);
 
-        // allow environment override via System env var if provided
-        // Grid environment
-        if (configurationReader.get("execution_env").equalsIgnoreCase("remote")) {
-            LoggerLoad.info("Running scripts in grid environment");
+		// allow environment override via System env var if provided
+		// Grid environment
+		if (configurationReader.get("execution_env").equalsIgnoreCase("remote")) {
+			LoggerLoad.info("Running scripts in grid environment");
 
-            // check if SELENIUM_HUB_URL is provided from Docker
-            String hubURL = System.getenv("SELENIUM_HUB_URL");
-            if (hubURL == null || hubURL.isEmpty()) {
-                hubURL = configurationReader.get("gridURL"); // fallback to config file
-            }
+			// check if SELENIUM_HUB_URL is provided from Docker
+			String hubURL = System.getenv("SELENIUM_HUB_URL");
+			if (hubURL == null || hubURL.isEmpty()) {
+				hubURL = configurationReader.get("gridURL"); // fallback to config file
+			}
 
-            LoggerLoad.info("Using Hub URL: " + hubURL);
-            DesiredCapabilities dcp = new DesiredCapabilities();
+			LoggerLoad.info("Using Hub URL: " + hubURL);
+			DesiredCapabilities dcp = new DesiredCapabilities();
 
-            switch (browser.toLowerCase()) {
-                case "chrome": {
-                    ChromeOptions co = new ChromeOptions();
-                    co.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
-                    co.setPageLoadStrategy(PageLoadStrategy.EAGER);
-                    co.addArguments("--start-maximized", "--remote-allow-origins=*");
-                    dcp.setCapability(ChromeOptions.CAPABILITY, co);
-                    dcp.setBrowserName("chrome");
-                    driver = new RemoteWebDriver(new URL(hubURL), dcp);
-                    setDriver(driver);
-                    break;
-                }
+			switch (browser.toLowerCase()) {
+			case "chrome": {
+				ChromeOptions co = new ChromeOptions();
+				co.setExperimentalOption("excludeSwitches", new String[] { "enable-automation" });
+				co.setPageLoadStrategy(PageLoadStrategy.EAGER);
 
-                case "edge": {
-                    EdgeOptions eo = new EdgeOptions();
-                    eo.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
-                    eo.setPageLoadStrategy(PageLoadStrategy.EAGER);
-                    dcp.setCapability(EdgeOptions.CAPABILITY, eo);
-                    dcp.setBrowserName("MicrosoftEdge");
-                    driver = new RemoteWebDriver(new URL(hubURL), dcp);
-                    setDriver(driver);
-                    break;
-                }
+				if (System.getenv("JENKINS_HOME") != null) {
+					co.addArguments("--headless=new");
+					co.addArguments("--disable-gpu");
+					co.addArguments("--no-sandbox");
+					co.addArguments("--window-size=1920,1080");
+				}
 
-                case "firefox": {
-                    FirefoxOptions fo = new FirefoxOptions();
-                    fo.setPageLoadStrategy(PageLoadStrategy.EAGER);
-                    dcp.setCapability(FirefoxOptions.FIREFOX_OPTIONS, fo);
-                    dcp.setBrowserName("firefox");
-                    driver = new RemoteWebDriver(new URL(hubURL), dcp);
-                    setDriver(driver);
-                    break;
-                }
+				co.addArguments("--remote-allow-origins=*");
 
-                default:
-                    throw new IllegalArgumentException("Choose a valid browser");
-            }
+				dcp.setCapability(ChromeOptions.CAPABILITY, co);
+				dcp.setBrowserName("chrome");
 
-            driver.manage().window().maximize();
-        }
+				driver = new RemoteWebDriver(new URL(hubURL), dcp);
+				setDriver(driver);
+				break;
+			}
 
-        // Local environment
-        else if (configurationReader.get("execution_env").equalsIgnoreCase("local")) {
-            LoggerLoad.info("Running scripts locally");
+			case "edge": {
+				EdgeOptions eo = new EdgeOptions();
+				eo.setExperimentalOption("excludeSwitches", new String[] { "enable-automation" });
+				eo.setPageLoadStrategy(PageLoadStrategy.EAGER);
 
-            switch (browser.toLowerCase()) {
-                case "chrome":
-                    WebDriverManager.chromedriver().setup();
-                    ChromeOptions co = new ChromeOptions();
-                    co.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
-                    co.setPageLoadStrategy(PageLoadStrategy.EAGER);
-                    co.addArguments("--start-maximized", "--remote-allow-origins=*");
-                    driver = new ChromeDriver(co);
-                    setDriver(driver);
-                    break;
+				if (System.getenv("JENKINS_HOME") != null) {
+					eo.addArguments("--headless=new");
+					eo.addArguments("--disable-gpu");
+					eo.addArguments("--no-sandbox");
+					eo.addArguments("--window-size=1920,1080");
+				}
 
-                case "firefox":
-                    WebDriverManager.firefoxdriver().setup();
-                    FirefoxOptions fo = new FirefoxOptions();
-                    fo.setPageLoadStrategy(PageLoadStrategy.EAGER);
-                    driver = new FirefoxDriver(fo);
-                    setDriver(driver);
-                    getDriver().manage().window().maximize();
-                    break;
+				dcp.setCapability(EdgeOptions.CAPABILITY, eo);
+				dcp.setBrowserName("MicrosoftEdge");
 
-                case "edge":
-                    WebDriverManager.edgedriver().setup();
-                    EdgeOptions eo = new EdgeOptions();
-                    eo.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
-                    eo.setPageLoadStrategy(PageLoadStrategy.EAGER);
-                    driver = new EdgeDriver(eo);
-                    setDriver(driver);
-                    getDriver().manage().window().maximize();
-                    break;
+				driver = new RemoteWebDriver(new URL(hubURL), dcp);
+				setDriver(driver);
+				break;
+			}
 
-                default:
-                    LoggerLoad.error("Unsupported browser provided: " + browser);
-                    throw new IllegalArgumentException("Browser not supported: " + browser);
-            }
-        }
+			case "firefox": {
+				FirefoxOptions fo = new FirefoxOptions();
+				fo.setPageLoadStrategy(PageLoadStrategy.EAGER);
 
-        // Common setup
-        getDriver().manage().deleteAllCookies();
-        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        getDriver().manage().timeouts().pageLoadTimeout(Duration.ofSeconds(40));
+				if (System.getenv("JENKINS_HOME") != null) {
+					fo.addArguments("-headless");
+				}
 
-        String url = configurationReader.get("baseURL");
-        LoggerLoad.info("Navigating to: " + url);
-        driver.get(url);
+				dcp.setCapability(FirefoxOptions.FIREFOX_OPTIONS, fo);
+				dcp.setBrowserName("firefox");
 
-        LoggerLoad.info("Writing Allure environment details...");
-        AllureEnvironmentWriter.writeEnv(driver);
+				driver = new RemoteWebDriver(new URL(hubURL), dcp);
+				setDriver(driver);
+				break;
+			}
 
-        LoggerLoad.info("Browser setup completed successfully.");
-    }
+			default:
+				throw new IllegalArgumentException("Choose a valid browser");
+			}
 
-    /** 
-     * @param result
-     */
-    @AfterMethod(alwaysRun = true)
-    public void attachLogsToAllure(ITestResult result) {
-        try {
-            File logFile = new File("logs/framework.log");
-            if (logFile.exists()) {
-                try (FileInputStream is = new FileInputStream(logFile)) {
-                    Allure.addAttachment(result.getMethod().getMethodName() + "-logs", is);
-                }
-            }
-        } catch (Exception e) {
-            System.err.println("Failed to attach logs: " + e.getMessage());
-        }
-    }
+			if (System.getenv("JENKINS_HOME") == null) {
+			    driver.manage().window().maximize();
+			}
 
-    @AfterTest(alwaysRun = true)
-    public void TearDown() {
+		}
 
-        if (getDriver() != null) {
-            LoggerLoad.info("Closing the browser and quitting WebDriver...");
-            try {
-                getDriver().quit();
-            } catch (Exception e) {
-                LoggerLoad.error("Error while quitting driver: " + e.getMessage());
-            } finally {
-                tlDriver.remove();
-            }
-            LoggerLoad.info("===== Test Execution Finished =====");
-        }
-    }
+		// Local environment
+		else if (configurationReader.get("execution_env").equalsIgnoreCase("local")) {
+			LoggerLoad.info("Running scripts locally");
+
+			switch (browser.toLowerCase()) {
+			case "chrome":
+				WebDriverManager.chromedriver().setup();
+				ChromeOptions co = new ChromeOptions();
+				co.setExperimentalOption("excludeSwitches", new String[] { "enable-automation" });
+				co.setPageLoadStrategy(PageLoadStrategy.EAGER);
+
+				if (System.getenv("JENKINS_HOME") != null) {
+					co.addArguments("--headless=new");
+					co.addArguments("--disable-gpu");
+					co.addArguments("--no-sandbox");
+					co.addArguments("--window-size=1920,1080");
+				} else {
+					co.addArguments("--start-maximized");
+				}
+
+				co.addArguments("--remote-allow-origins=*");
+
+				driver = new ChromeDriver(co);
+				setDriver(driver);
+				break;
+
+			case "firefox":
+				WebDriverManager.firefoxdriver().setup();
+				FirefoxOptions fo = new FirefoxOptions();
+				fo.setPageLoadStrategy(PageLoadStrategy.EAGER);
+
+				if (System.getenv("JENKINS_HOME") != null) {
+					fo.addArguments("-headless");
+				}
+
+				driver = new FirefoxDriver(fo);
+				setDriver(driver);
+				break;
+
+			case "edge":
+				WebDriverManager.edgedriver().setup();
+				EdgeOptions eo = new EdgeOptions();
+				eo.setExperimentalOption("excludeSwitches", new String[] { "enable-automation" });
+				eo.setPageLoadStrategy(PageLoadStrategy.EAGER);
+
+				if (System.getenv("JENKINS_HOME") != null) {
+					eo.addArguments("--headless=new");
+					eo.addArguments("--disable-gpu");
+					eo.addArguments("--no-sandbox");
+					eo.addArguments("--window-size=1920,1080");
+				}
+
+				driver = new EdgeDriver(eo);
+				setDriver(driver);
+				break;
+
+			default:
+				LoggerLoad.error("Unsupported browser provided: " + browser);
+				throw new IllegalArgumentException("Browser not supported: " + browser);
+			}
+		}
+
+		// Common setup
+		getDriver().manage().deleteAllCookies();
+		getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		getDriver().manage().timeouts().pageLoadTimeout(Duration.ofSeconds(40));
+
+		String url = configurationReader.get("baseURL");
+		LoggerLoad.info("Navigating to: " + url);
+		driver.get(url);
+
+		LoggerLoad.info("Writing Allure environment details...");
+		AllureEnvironmentWriter.writeEnv(driver);
+
+		LoggerLoad.info("Browser setup completed successfully.");
+	}
+
+	/**
+	 * @param result
+	 */
+	@AfterMethod(alwaysRun = true)
+	public void attachLogsToAllure(ITestResult result) {
+		try {
+			File logFile = new File("logs/framework.log");
+			if (logFile.exists()) {
+				try (FileInputStream is = new FileInputStream(logFile)) {
+					Allure.addAttachment(result.getMethod().getMethodName() + "-logs", is);
+				}
+			}
+		} catch (Exception e) {
+			System.err.println("Failed to attach logs: " + e.getMessage());
+		}
+	}
+
+	@AfterTest(alwaysRun = true)
+	public void TearDown() {
+
+		if (getDriver() != null) {
+			LoggerLoad.info("Closing the browser and quitting WebDriver...");
+			try {
+				getDriver().quit();
+			} catch (Exception e) {
+				LoggerLoad.error("Error while quitting driver: " + e.getMessage());
+			} finally {
+				tlDriver.remove();
+			}
+			LoggerLoad.info("===== Test Execution Finished =====");
+		}
+	}
 }
