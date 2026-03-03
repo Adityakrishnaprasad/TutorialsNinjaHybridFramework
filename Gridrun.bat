b16b325a31e474bd599bed58f40f381326212be9
Adityarockyroy <adityarockyroy136@gmail.com>
2026-03-03 16:41:23 +0530

    ModifiedCodesForLocalExecution

--- Gridrun.bat
+++ Gridrun.bat
@@ -1,1 +1,23 @@
-java -jar selenium-server-4.36.0.jar standalone --selenium-manager true
+@echo off
+
+echo =====================================
+echo Starting Selenium Grid...
+echo =====================================
+docker compose up -d
+
+echo Waiting for Grid to be ready...
+timeout /t 5
+
+echo =====================================
+echo Running TestNG Suite...
+echo =====================================
+mvn clean test -Dsurefire.suiteXmlFiles=grid-parallel-suite.xml
+echo =====================================
+echo Stopping Selenium Grid...
+echo =====================================
+docker compose down
+
+echo =====================================
+echo Execution Finished
+echo =====================================
+pause
